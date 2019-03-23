aae94c375d644fb42d22bf7b6ce7005905a88a49
juthomas <juthomas@student.42.fr>
2019-03-23 17:06:55 +0100

    [ADD] cleaning gpio using Ctrl+C

--- C_use/Led/srcs/entry/entry.c
+++ C_use/Led/srcs/entry/entry.c
@@ -1,42 +1,59 @@
 #include <wiringPi.h>
 #include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
 
 int ledPin = 29;  //Correspond à la pin 40
-int black = 0;
-int red = 2;
-int white = 3;
+int black = 0;    //Correspond à la pin 11
+int red = 2;      //Correspond à la pin 13
+int white = 3;    //Correspond à la pin 15
 
-void setup(){
-
+void setup()
+{
+	pinMode(ledPin,OUTPUT);
 	pinMode(black, OUTPUT);
 	pinMode(red, OUTPUT);
 	pinMode(white, OUTPUT);
-	pinMode(ledPin,OUTPUT);
 	printf("Hello World\n");
 	digitalWrite(black, LOW);
 	digitalWrite(red, 500);
 }
 
-void loop(){
+void loop()
+{
 	digitalWrite(ledPin,HIGH);
 	delay(100);
 	digitalWrite(ledPin,LOW);
 	delay(100);
-
 	printf("Blink the LED\n");
+}
+
+void clear_gpio (int sig)
+{
+	printf("Signal : %d\n", sig);
+	printf("Cleaning used Gpio\n");
+	pinMode(ledPin, INPUT);
+	pinMode(black, INPUT);
+	pinMode(red, INPUT);
+	pinMode(white, INPUT);
+	exit(1);
 }
 
 int main(void)//(int argc, char **argv)
 {
-	if(wiringPiSetup()<0){
-
-	printf("setup wiring pi failed\n");
-		return 1;
+	//signal(SIGQUIT, clear_gpio);
+	signal(SIGINT, clear_gpio);
+	if(wiringPiSetup() < 0)
+	{
+		printf("Setup wiringPi failed\n");
+		return (1);
 	}
 	setup();
-	while(1){
+	while(1)
+	{
 		loop();
 	}
-	
-	return 0;
+	clear_gpio(0);
+	return (0);
 }
+
--- Cpp_use/Led/srcs/entry/entry.cpp
+++ Cpp_use/Led/srcs/entry/entry.cpp
@@ -1,42 +1,59 @@
 #include <iostream>
 #include <wiringPi.h>
+#include <signal.h>
+
 using namespace std;
 
 int ledPin = 29;  //Correspond à la pin 40
-int black = 0;
-int red = 2;
-int white = 3;
+int black = 0;    //Correspond à la pin 11
+int red = 2;      //Correspond à la pin 13
+int white = 3;    //Correspond à la pin 15
 
-void setup(){
-
+void setup()
+{
+	pinMode(ledPin,OUTPUT);
 	pinMode(black, OUTPUT);
 	pinMode(red, OUTPUT);
 	pinMode(white, OUTPUT);
-	pinMode(ledPin,OUTPUT);
-	cout<<"Hello World" << endl;
+	cout << "Hello World" << endl;
 	digitalWrite(black, LOW);
 	digitalWrite(red, 500);
 }
 
-void loop(){
+void loop()
+{
 	digitalWrite(ledPin,HIGH);
 	delay(100);
 	digitalWrite(ledPin,LOW);
 	delay(100);
+	cout << "Blink the LED" << endl;
+}
 
-	cout<<"Blink the LED" << endl;
+void clear_gpio(int sig)
+{
+
+	cout << "Signal : " << sig << endl;
+	cout << "Cleaning used Gpio" << endl;
+	pinMode(ledPin, INPUT);
+	pinMode(black, INPUT);
+	pinMode(red, INPUT);
+	pinMode(white, INPUT);
+	exit(1);
 }
 
 int main(void)//(int argc, char **argv)
 {
-	if(wiringPiSetup()<0){
-		cout<<"setup wiring pi failed"<<endl;
-		return 1;
+	signal(SIGINT, clear_gpio);
+	if (wiringPiSetup() < 0)
+	{
+		cout << "Setup wiringPi failed" << endl;
+		return(1);
 	}
 	setup();
-	while(1){
+	while (1)
+	{
 		loop();
 	}
-	
-	return 0;
+	clear_gpio(0);
+	return(0);
 }
